f9785462c8e90984eb1084787e1d699626eb28b0
rDibo <ricardodibo@gmail.com>
2025-05-14 22:02:03 -0300

    CSS vars and resets

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ function App() {
   return (
     <>
       <Heading>Components with Props</Heading>
-      <Heading atrributes={123}>Components with Props</Heading>
-      <Heading atrributes="String">Components with Props</Heading>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id a, qui nesciunt nam inventore culpa nobis magni, voluptatum incidunt unde totam et sint repellendus deserunt repellat possimus nemo recusandae fugiat?
       </p>

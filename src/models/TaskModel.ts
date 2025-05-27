22c233c861dd2c1ea941b0365b71f33c7dd49099
rDibo <ricardodibo@gmail.com>
2025-05-26 21:20:58 -0300

    Implemented models.

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,12 @@
+import type { TaskStateModel } from "./TaskStateModel";
+
+export type TaskModel = {
+  id: string;
+  name: string;
+  duration: number;
+  startDate: number;
+  endDate: number;
+  completedDate: number | null;
+  interruptDate: number | null;
+  type: keyof TaskStateModel['config'];
+}
